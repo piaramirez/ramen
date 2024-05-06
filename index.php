da73340551761399ff83312abc8b91ff1c7d0638
Pía Ramírez <the.captain312@gmail.com>
2024-05-05 20:00:20 -0600

    Empezamos a crear los controladores y terminamos de configurar las url para recibir los datos que iran al parámetro, métodos y controladores

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+    $url = !empty( $_GET['url']) ? $_GET['url']: 'home/home';
+    $arrUrl = explode("/", $url);
+    $controller  = $arrUrl[0];
+    $metodo = $arrUrl[0];
+    $param = "";
+    if(!empty( $arrUrl[1])){
+        if( $arrUrl[1] != ""){
+            $metodo = $arrUrl[1];
+        }
+    }
+    if(!empty($arrUrl[2])){
+        if($arrUrl[2] != ""){
+            for($i=2; $i<count($arrUrl); $i++){
+                $param.= $arrUrl[$i].',';
+            }
+            $param = trim($param, ',');
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
