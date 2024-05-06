6bda05348529665f8d7232672a85fb92b5f2baba
Pía Ramírez <the.captain312@gmail.com>
2024-05-05 20:18:13 -0600

    Solucionando bugs del Load

---
+++ Config/Config.php
@@ -0,0 +1,6 @@
+<?php
+//define("BASE_URL", "http://localhost/soraRamen/");
+const BASE_URL = "http://localhost/soraRamen/";
+const LIBS = "Assets/Libraries/";
+const VIEWS = "Views/";
+//echo "Estamos dentro";
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once("Config/Config.php");
     $url = !empty( $_GET['url']) ? $_GET['url']: 'home/home';
     $arrUrl = explode("/", $url);
     $controller  = $arrUrl[0];
@@ -16,6 +17,19 @@
             }
             $param = trim($param, ',');
         }
+    }
+    spl_autoload_register(function($class){
+        
+        if (file_exists(LIBS."Core/".$class.".php")) {
+            require_once(LIBS."Core/".$class.".php");
+        }
+    });
+    //Load
+    $controllerFile =  "Controllers/".$LIBS."Core/".$class."php".".php";
+    if(file_exists($controllerFile)){
+        echo "w";
+    }else{
+        echo "No existe el controlador";
     }
 ?>
 <!DOCTYPE html>
